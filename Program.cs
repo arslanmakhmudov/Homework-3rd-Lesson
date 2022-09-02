2a441acfee5cdb315873d2ef7352d0e9bb8bbb55
Arslan <anderfell2003@mail.ru>
2022-09-02 19:34:37 +0500

    Main Commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 19
+
+// Console.WriteLine("Введите пятизначное число");
+// string number = Convert.ToString(Console.ReadLine());
+// if (number [0] == number [4] && number [1] == number [3])
+// {
+//     Console.WriteLine("Ваше число" + number + " является палиндромом");
+// }
+// else {Console.WriteLine("Ваше число не является палиндромом");}
+
+// Задача 21
+
+// Console.WriteLine("Укажите координаты точки А");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// int z1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Укажите координаты точки B");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// int z2 = Convert.ToInt32(Console.ReadLine());
+
+// double D = Math.Sqrt(Math.Pow(x2 - x1, 2)) + (Math.Pow(y2 - y1, 2)) + (Math.Pow(z2 - z1, 2));
+// System.Console.WriteLine(D); 
+
+// Задача 23
+
+// Console.WriteLine("Введите число: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// for (int count = 0; count <= N; count++)
+// {
+//     Console.WriteLine($"Куб от числа {count} = {Math.Pow(count, 3)}");
+// }
